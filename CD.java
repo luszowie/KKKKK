aac14935d6615bcaea92167caeb68c956d430607
luszowie <luszowie@wszib.edu.pl>
2019-01-12 17:14:03 +0100

    frg

---
+++ CD.java
@@ -0,0 +1,25 @@
+public class CD extends MusicPlayer {
+    public CD() {
+    }
+
+    @Override
+    public void odtwarzaj() {
+
+    }
+
+    @Override
+    public void PauseStop() {
+
+    }
+
+    @Override
+    public void NextSong() {
+
+    }
+
+    @Override
+    public void PreviousSong() {
+
+    }
+}
+
